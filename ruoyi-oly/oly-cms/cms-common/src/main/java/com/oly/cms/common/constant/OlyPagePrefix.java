7fb08f82afe2cad6170e99c0e4f22ff69d464aa8
hush <634866553@qq.com>
2022-08-15 15:08:34 +0800

    去除前缀

--- ruoyi-oly/oly-cms/cms-common/src/main/java/com/oly/cms/common/constant/OlyPagePrefix.java
+++ ruoyi-oly/oly-cms/cms-common/src/main/java/com/oly/cms/common/constant/OlyPagePrefix.java
@@ -5,22 +5,17 @@ package com.oly.cms.common.constant;
  */
 public class OlyPagePrefix {
 
-    public static final String indexPage = "/web";
-    public static final String postsPage = "/web/post";
-    public static final String postPage = "/web/post/";
-    public static final String catsPage = "/web/cat";
-    public static final String catPage = "/web/cat/";
-    public static final String tagsPage = "/web/tag";
-    public static final String tagPage = "/web/tag/";
-    public static final String aboutPage = "/web/about";
-    public static final String rankPage = "/web/rank";
-    public static final String linksPage = "/web/links";
-    public static final String contactPage = "/web/contact";
+    public static final String indexPage = "";
+    public static final String postsPage = "/post";
+    public static final String postPage = "/post/";
+    public static final String catsPage = "/cat";
+    public static final String catPage = "/cat/";
+    public static final String tagsPage = "/tag";
+    public static final String tagPage = "/tag/";
+    public static final String aboutPage = "/about";
+    public static final String rankPage = "/rank";
+    public static final String linksPage = "/links";
+    public static final String contactPage = "/contact";
     // 以上博客|综合页面
-
-    public static final String userLoginPage = "/web/user/login";
-    public static final String userRegPage = "/web/user/register";
-    public static final String userPersonalPage = "/web/user/personal";
-    // 以上用户个人中心
 
 }
