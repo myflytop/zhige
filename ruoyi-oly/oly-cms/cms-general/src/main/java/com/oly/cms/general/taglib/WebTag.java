9a794a88b910b68dd38921bf9a93f659a43257d1
hush <634866553@qq.com>
2022-08-22 18:03:50 +0800

    添加新方法，用于验证类型是否支持主题

---
+++ ruoyi-oly/oly-cms/cms-general/src/main/java/com/oly/cms/general/taglib/WebTag.java
@@ -0,0 +1,37 @@
+package com.oly.cms.general.taglib;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.oly.cms.common.model.properties.OlyWebConfigProperties;
+import com.ruoyi.common.core.text.Convert;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.system.config.service.ISysConfigService;
+
+@Service
+public class WebTag {
+
+    @Autowired
+    private ISysConfigService configService;
+
+    /**
+     * 获取文章支持类型
+     * 返回空或者长度为0表示支持所有
+     * 
+     * @param themeName
+     * @return
+     */
+    public Integer[] getThemeSupportType(String themeName) {
+        return Convert.toIntArray(",",
+                configService.selectConfigDefauleValue(themeName, OlyWebConfigProperties.ARTICLE_TYPES));
+    }
+
+    public boolean checkTagSupportTheme(int type, String themeName) {
+        if (StringUtils.isEmpty(getThemeSupportType(themeName))) {
+            return true;
+        }
+        return Arrays.asList(getThemeSupportType(themeName)).contains(type);
+    }
+
+}
