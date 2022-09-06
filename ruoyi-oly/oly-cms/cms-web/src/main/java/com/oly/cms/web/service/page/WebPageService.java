0b974a45602c53ed712d377ab0efaf5d15360de3
hush <634866553@qq.com>
2022-09-06 22:28:28 +0800

    如果文章支持类型,则对应标签,分类关联文章均支持文章

--- ruoyi-oly/oly-cms/cms-web/src/main/java/com/oly/cms/web/service/page/WebPageService.java
+++ ruoyi-oly/oly-cms/cms-web/src/main/java/com/oly/cms/web/service/page/WebPageService.java
@@ -113,7 +113,6 @@ public class WebPageService extends CommonController {
             if (StringUtils.isEmpty(types) || ArrayUtils.contains(types, tag.getTagType().toString())) {
                 WebArticleSearchParam bl = new WebArticleSearchParam();
                 bl.setTagId(tagId);
-                bl.setThemeName(themeName);
                 List<WebArticle> list = new ArrayList<>();
                 startPage();
                 list = webPostService.listWebArticles(bl);
@@ -159,7 +158,6 @@ public class WebPageService extends CommonController {
             if (StringUtils.isEmpty(types) || ArrayUtils.contains(types, cat.getCatType().toString())) {
                 WebArticleSearchParam bl = new WebArticleSearchParam();
                 bl.setCatId(catId);
-                bl.setThemeName(themeName);
                 List<WebArticle> list = new ArrayList<>();
                 startPage();
                 list = webPostService.listWebArticles(bl);
